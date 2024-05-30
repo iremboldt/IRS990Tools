25287f945b7de0521f489c7039fa7eca5285c5be
iremboldt <isaacremboldt@gmail.com>
2024-05-30 16:23:47 -0500

    Update XMLStateSearch.py
    Updated script to include missing import statement and removing double quotes from the word processor formatting

--- XMLStateSearch.py
+++ XMLStateSearch.py
@@ -1,17 +1,18 @@
 import os
+import xml.etree.ElementTree as ET
 
-directory = os.fsencode(“directory location”)
-directorystring = “directory location”
+directory = os.fsencode('directory location')
+directorystring = 'directory location'
 
 for file in os.listdir(directory):
     filename = os.fsencode(file)
-    tree = ET.parse(directorystring+”\\”+filename)
+    tree = ET.parse(directorystring+'\\'+filename)
     root = tree.getroot()
     state = root[0][5][4][2].text
     try:
         if state != ‘NE’:
-            os.remove(directorystring+”\\”+filename)
+            os.remove(directorystring+'\\'+filename)
         else:
             continue
     except:
-        print(‘child index out of range for ‘+filename)
+        print('child index out of range for '+filename)
