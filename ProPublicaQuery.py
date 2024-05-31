95c726a33646983f07c8b1c5ec8afdd29180e2bd
iremboldt <isaacremboldt@gmail.com>
2024-05-31 16:05:28 -0500

    Update ProPublicaQuery.py
    Accidentally didn't save the last one, added a list and a for loop for messing with later

--- ProPublicaQuery.py
+++ ProPublicaQuery.py
@@ -5,3 +5,8 @@ eid = ''
 url = r'https://projects.propublica.org/nonprofits/api/v2/organizations/'
 response = requests.get(url+eid+'.json')
 print(response.text)
+
+list = []
+for item in list:
+  response = requests.get(url+item)
+  print(response.text)
