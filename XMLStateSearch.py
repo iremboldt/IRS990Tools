42160fc2dbf391670625fe8ea7ae2a7a9742eb24
iremboldt <isaacremboldt@gmail.com>
2024-06-10 16:12:53 -0500

    Update XMLStateSearch.py
    added functions to this script, added code as a function for downloading and extracting .zip files

--- XMLStateSearch.py
+++ XMLStateSearch.py
@@ -2,9 +2,16 @@ import os
 import xml.etree.ElementTree as ET
 #This uses Element Tree, an XML parser library for python
 
-#Set the location of the directory as a string, then create an os object
+#https://www.irs.gov/charities-non-profits/form-990-series-downloads
+#Set the location of the directory the 990s were, or will be, saved in as a string, then create an os object
 path = r'directory location'
 directory = os.fsencode(path)
+
+def get990s(year)
+    monthcodes = ['01A','01B','01C','02A','02B','02C','03A','03B','03C','04A','04B','04C','05A','05B','05C','06A','06B','06C','07A','07B','07C','08A','08B','08C','09A','09B','09C','10A','10B','10C','11A','11B','11C','12A','12B','12C']
+    for month in monthcodes:
+        #save the .zip for each one, potentially even extract the data as well in this function
+        'https://apps.irs.gov/pub/epostcard/990/xml/'+year+'/'+year+'_TEOS_XML_'+month+'.zip'
 
 #iterate through files in the directory
 for file in os.listdir(directory):
