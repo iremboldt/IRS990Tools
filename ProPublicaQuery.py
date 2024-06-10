9900f81b8e2b410c9c3d38239e493d252193b3e1
iremboldt <isaacremboldt@gmail.com>
2024-06-10 15:57:25 -0500

    Update ProPublicaQuery.py
    Created functions so that they can be called easier in the future.

--- ProPublicaQuery.py
+++ ProPublicaQuery.py
@@ -1,12 +1,22 @@
 import requests
 
-eid = ''
+#add a string to the end to return a .json with data for that organization
+NameSearchURL = r'https://projects.propublica.org/nonprofits/api/v2/search.json?q='
+#add "EIN".json to the end of the url to get 990 info for the org. ex. organizations/123456789.json
+OrgSearchURL = r'https://projects.propublica.org/nonprofits/api/v2/organizations/'
 
-url = r'https://projects.propublica.org/nonprofits/api/v2/organizations/'
-response = requests.get(url+eid+'.json')
-print(response.text)
+eids = []
+names = []
 
-list = []
-for item in list:
-  response = requests.get(url+item)
-  print(response.text)
+def getEIDs(names):
+for name in names:
+  response = requests.get(NameSearchURL+name)
+  #Parse the returned json for the EID and append to eids list
+return(eids)
+
+def getTaxInfo(eids):
+TaxInfo = []
+for eid in eids:
+  response = requests.get(OrgSearchURL+eid+'.json')
+  #Parse the json and append data to the list, or create a series of csvs
+return(TaxInfo)
