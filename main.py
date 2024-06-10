795601195ca716d725fccc445d90a7b3348a5d41
iremboldt <isaacremboldt@gmail.com>
2024-06-10 15:03:15 -0500

    Create main.py
    Read a geeks for geeks article and it clicked so I'm just going to do it "legit" now...

---
+++ main.py
@@ -0,0 +1,10 @@
+# A Sample class with init method
+class main:
+ 
+    # init method or constructor
+    def __init__(self, name):
+        self.name = name
+ 
+    # Sample Method
+    def say_hi(self):
+        print('Hello, my name is', self.name)
