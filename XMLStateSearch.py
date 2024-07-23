0582a26655d02118bd7d57ec850e5649220a0f59
iremboldt <isaacremboldt@gmail.com>
2024-07-22 19:56:12 -0500

    Update XMLStateSearch.py

--- XMLStateSearch.py
+++ XMLStateSearch.py
@@ -8,19 +8,19 @@ from zipfile import ZipFile
 path = r'directory location'
 directory = os.fsencode(path)
 
-def get990s(year)
+def get990s(year):
     monthcodes = ['01A','01B','01C','02A','02B','02C','03A','03B','03C','04A','04B','04C','05A','05B','05C','06A','06B','06C','07A','07B','07C','08A','08B','08C','09A','09B','09C','10A','10B','10C','11A','11B','11C','12A','12B','12C']
     for month in monthcodes:
         #save the .zip for each one, potentially even extract the data as well in this function
         'https://apps.irs.gov/pub/epostcard/990/xml/'+year+'/'+year+'_TEOS_XML_'+month+'.zip'
 
 # input is zip file location, output is desired extract location
-def extractzip(input,output)
+def extractzip(input,output):
 with ZipFile(input, 'r') as zObject: 
     zObject.extractall(path=output) 
 
 #iterate through files in the directory and remove non Nebraska States
-def removeStates(directory)
+def removeStates(directory):
     for file in os.listdir(directory):
         filename = os.fsencode(file)
         tree = ET.parse(path+'\\'+filename)
@@ -35,7 +35,7 @@ def removeStates(directory)
         except:
             print('child index out of range for '+filename)
 
-def getEIN(directory)
+def getEIN(directory):
     eidList=[]
     for file in os.listdir(directory):
         filename = os.fsencode(file)
