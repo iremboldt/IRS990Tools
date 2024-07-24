599ec89d708065cd0bd04a6ca5ccc044e9d5ffe2
iremboldt <isaacremboldt@gmail.com>
2024-07-24 10:02:44 -0500

    Update XMLStateSearch.py
    Nested Try statements because if the file was a nebraska file, but didnt have the state code in the correct location, it got deleted anyway. This requires manual review of files that show ip in the exceptions list

--- XMLStateSearch.py
+++ XMLStateSearch.py
@@ -28,16 +28,16 @@ def removeStates(directory):
         #the following code replaced trying to search by index, and I just put in the xml path. the part that threw me off was starting with the irs url each time.
         try:
             state = tree.find('.//{http://www.irs.gov/efile}ReturnHeader/{http://www.irs.gov/efile}Filer/{http://www.irs.gov/efile}USAddress/{http://www.irs.gov/efile}StateAbbreviationCd').text
+            try:
+                if state != ‘NE’:
+                    os.remove(directory+'\\'+filename)
+                else:
+                    continue
+            except:
+                print('child index out of range for '+filename)
         except:
             print('State not found at expected index for '+filename)
         #put in a try catch because the previous three strings kept messing up, also the XMLs have slight variations and I wanted a log of which ones to do manually or with an adjusted script
-        try:
-            if state != ‘NE’:
-                os.remove(directory+'\\'+filename)
-            else:
-                continue
-        except:
-            print('child index out of range for '+filename)
 
 #This method is developed from the removeStates function, but it extracts EINs from the expected xml path and adds them to a list
 def getEIN(directory):
