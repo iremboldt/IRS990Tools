b907609c02b991e9e2bfe84dd4c48d9c3b5e9d8e
iremboldt <isaacremboldt@gmail.com>
2024-07-28 13:21:12 -0500

    Update XMLStateSearch.py

--- XMLStateSearch.py
+++ XMLStateSearch.py
@@ -9,16 +9,28 @@ import pandas as pd
 path = r'directory location'
 directory = os.fsencode(path)
 
-def get990s(year):
-    monthcodes = ['01A','01B','01C','02A','02B','02C','03A','03B','03C','04A','04B','04C','05A','05B','05C','06A','06B','06C','07A','07B','07C','08A','08B','08C','09A','09B','09C','10A','10B','10C','11A','11B','11C','12A','12B','12C']
-    for month in monthcodes:
-        #save the .zip for each one, potentially even extract the data as well in this function
-        'https://apps.irs.gov/pub/epostcard/990/xml/'+year+'/'+year+'_TEOS_XML_'+month+'.zip'
-
 # input is zip file location, output is desired extract location
 def extractzip(input,output):
 with ZipFile(input, 'r') as zObject: 
     zObject.extractall(path=output) 
+    
+def get990s(extractYN,directory,*years):
+    for year in years:
+        monthcodes = ['01A','01B','01C','02A','02B','02C','03A','03B','03C','04A','04B','04C','05A','05B','05C','06A','06B','06C','07A','07B','07C','08A','08B','08C','09A','09B','09C','10A','10B','10C','11A','11B','11C','12A','12B','12C']
+        for month in monthcodes:
+            #save the .zip for each one, potentially even extract the data as well in this function
+            url = 'https://apps.irs.gov/pub/epostcard/990/xml/'+year+'/'+year+'_TEOS_XML_'+month+'.zip'
+            file = requests.get(url, stream=True
+            dump = file.raw
+            location = os.path.abspath(directory)
+            filename = 'returns'+month+year+'.zip'
+            with open(filename, 'wb') as location:
+                shutil.copyfileobj(dump, location)
+            del dump
+            if extractYN == True:
+                extractzip(directory+filename)
+            else:
+                continue
 
 #iterate through files in the directory and remove non Nebraska States
 def removeStates(directory):
