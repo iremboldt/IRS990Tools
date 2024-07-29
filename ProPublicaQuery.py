228babe7923cc419287dae02892acd118a824e45
iremboldt <isaacremboldt@gmail.com>
2024-07-29 08:35:59 -0500

    Update ProPublicaQuery.py

--- ProPublicaQuery.py
+++ ProPublicaQuery.py
@@ -1,45 +1,41 @@
-import requests
+import requests 
+import csv
+import json
+import pandas as pd
 
-#add a string to the end to return a .json with data for that organization
-NameSearchURL = r'https://projects.propublica.org/nonprofits/api/v2/search.json?q='
-#add "EIN".json to the end of the url to get 990 info for the org. ex. organizations/123456789.json
-OrgSearchURL = r'https://projects.propublica.org/nonprofits/api/v2/organizations/'
-
-eins = []
-names = []
+def __init__(self,eins,names)
+  #add a string to the end to return a .json with data for that organization
+  NameSearchURL = r'https://projects.propublica.org/nonprofits/api/v2/search.json?q='
+  #add "EIN".json to the end of the url to get 990 info for the org. ex. organizations/123456789.json
+  OrgSearchURL = r'https://projects.propublica.org/nonprofits/api/v2/organizations/'
+  #These should be input as python lists, consider using *args to allow multiple inputs, but for now this should work.
+  eins = self.eins
+  names = self.names
 
 def geteins(names):
-for name in names:
-  response = requests.get(NameSearchURL+name)
-  #Parse the returned json for the ein and append to eins list
-return(eins)
+  for name in names:
+    response = requests.get(NameSearchURL+name)
+    #Parse the returned json for the ein and append to eins list
+  return(eins)
 
 def getTaxInfo(eins):
-TaxInfo = []
-for ein in eins:
-  response = requests.get(OrgSearchURL+ein+'.json')
-  #Parse the json and append data to the list, or create a series of csvs
-return(TaxInfo)
+    TaxInfo = []
+    for ein in eins:
+      response = requests.get(OrgSearchURL+ein+'.json')
+      #Parse the json and append data to the list, or create a series of csvs
+      return(TaxInfo)
 
 def writeCSV():
-    import csv
-  import json
-  import requests
-  import pandas as pd
-  einList=[]
-  #populate nplist with values from non profit list, url encoded
-  nplist=[]
-  
   #x=json.loads(response.content)
   #for x in x:
-    #https://stackoverflow.com/questions/1871524/how-can-i-convert-json-to-csv
-    #f.writerow([x["Organizations"]["ein"])
+  #https://stackoverflow.com/questions/1871524/how-can-i-convert-json-to-csv
+  #f.writerow([x["Organizations"]["ein"])
   
   #got this to work!!
-  #the list i got sewhat differs from name, eg ampersand being spelled out
+  #the list i got somewhat differs from name, eg ampersand being spelled out
   data=pd.read_json(json.dumps(jsonstring))
-  for np in nplist:
-      response=requests.get('https://projects.propublica.org/nonprofits/api/v2/search.json?q='+np)
+  for name in names:
+      response=requests.get('https://projects.propublica.org/nonprofits/api/v2/search.json?q='+name)
       npjson=json.loads(response.content)
       data=pd.read_json(json.dumps(npjspn))
       if len(data.index)>1:
@@ -47,14 +43,14 @@ def writeCSV():
         while i<len(data.index):
           row=data['organizations'].loc[data.index[i]]
           try:
-              einList.append(row['ein'])
+              eins.append(row['ein'])
           except:
               print('ein does not exist for '+np)
           i+=1
       elif len(data.index)==1:
         row=data['organization'].loc[data.index[0]]
         try:
-            einList.append(row['ein'])
+            eins.append(row['ein'])
         except:
             print('ein does not exist for '+np)
       else:
