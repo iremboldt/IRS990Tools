d508e56c9e6d0da0fade13de06a5328cfc3ec872
iremboldt <isaacremboldt@gmail.com>
2024-08-13 13:42:07 -0500

    Update main.py
    main.py that does all the functions in the 990 tools along with user inputs

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-# A Sample class with init method
-class main:
- 
-    # init method or constructor
-    def __init__(self, name):
-        self.name = name
- 
-    # Sample Method
-    def say_hi(self):
-        print('Hello, my name is', self.name)
+import os
+import xml.etree.ElementTree as ET
+import zipfile
+import pandas as pd
+import requests
+import urllib.request
+
+directory=input("Enter target directory: ")
+IRSTools=IRS990Tools(directory)
+#print("Would you like to enter more than one year? Y/N")
+#choice=input("")
+#if choice == "Y" or "N":
+# if choice == "Y":
+years=input("Enter target year: ")
+IRSTools.get990s(years)
+IRSTools.extractzip():
+statecode=input("Enter target state code: ")
+statecode=statecode.upper()
+if statecode.len() > 2:
+ statecode=input("State code must be two characters, try again: ")
+IRSTools.removeStates(statecode)
+citycode=input("Enter target city: ")
+IRSTools.removeCity(citycode)
+IRSTools.xmltocsv()
